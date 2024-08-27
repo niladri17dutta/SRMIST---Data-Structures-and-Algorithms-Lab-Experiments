4708aa0aeaca2b86570c413a267157d2ab21aa87
niladri17dutta <niladridutta.2005@gmail.com>
2024-08-28 00:43:26 +0530

    Implementation of Doubly Linked List

---
+++ week6_Implementation_of_Doubly_linked_List.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->prev = nullptr;
+        newNode->next = nullptr;
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void displayForward() {
+        Node* temp = head;
+        cout << "List in forward order: ";
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void displayBackward() {
+        Node* temp = tail;
+        cout << "List in backward order: ";
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+
+    ~DoublyLinkedList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+    int n, value;
+
+    cout << "Enter the number of elements you want to insert: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter value for element " << i + 1 << ": ";
+        cin >> value;
+        dll.insert(value);
+    }
+
+    dll.displayForward();
+    dll.displayBackward();
+
+    return 0;
+}
